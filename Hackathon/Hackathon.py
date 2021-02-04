7a643f734b65439a4c00ada49bf2bb8bca7c7988
dnwjddl <woojung980305@gmail.com>
2021-02-04 23:57:20 +0900

    timer

--- Hackathon/Hackathon.py
+++ Hackathon/Hackathon.py
@@ -1,7 +1,10 @@
 import cv2
 import mediapipe as mp
 import math
-import threading
+import time
+
+times = 10
+
 
 def get_angle(left, right):
     x = right.x - left.x
@@ -14,27 +17,39 @@ def get_angle(left, right):
     else:
         print("이사람 정자세 아니다!!!!!!!!!!")
 
+
 def is_turtleneck(ear, shoulder):
     # print(round(ear,2), round(shoulder,2))
     turtleneck = shoulder * 0.1
 
-    if shoulder - turtleneck < ear <  shoulder + turtleneck:
+    if shoulder - turtleneck < ear < shoulder + turtleneck:
         print("거북목 아님")
     else:
         print("거북목 의심해보시길...")
 
 
 def main():
-
-
     mp_drawing = mp.solutions.drawing_utils
     mp_pose = mp.solutions.pose
 
-    #웹캠으로 사용
-    pose = mp_pose.Pose( min_detection_confidence=0.5)
+    # 웹캠으로 사용
+    pose = mp_pose.Pose(min_detection_confidence=0.5)
 
     cap = cv2.VideoCapture(0)
+    prevTime = time.time()
+    print("TIMER START")
+
     while cap.isOpened():
+        global times
+        curTime = time.time()
+        sec = curTime - prevTime
+        prevTime = curTime
+        times -= sec
+        if times < 0:
+            print("alert!!!!!!!!!!!!!")
+
+        # print("sec", sec)
+
         success, image = cap.read()
         if not success:
             print("카메라가 인식되지않습니다.")
@@ -44,18 +59,16 @@ def main():
         # 해당 영역에서 입력된 이미지를 분석하여 결과값을 도출해줍니다.
         results = pose.process(image)
 
+        '''
         try:
             if results.pose_landmarks.landmark[23].y > 1:
-
                 print("이사람 앉아있다!!!!")
-                if is_sitting == False:
-                    is_sitting = True
-                    startTimer()
 
             else:
                 print("이사람 서있다!!!!!!!!!!")
-                    
-            '''
+        '''
+
+        '''
             #================거북목?======================
             l_ear = results.pose_landmarks.landmark[7].z
             l_sh = results.pose_landmarks.landmark[11].z
@@ -67,12 +80,11 @@ def main():
             get_angle(l_sh, r_sh)
             #================앉아있는지?==================
             print(results.pose_landmarks.landmark[23].y)
-            
-            '''
-        except Exception as e:
-            print(e)
 
-        
+        '''
+
+        # except Exception as e:
+        #    print(e)
 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -86,6 +98,7 @@ def main():
             break
     pose.close()
     cap.release()
+
 
 if __name__ == "__main__":
     main()
